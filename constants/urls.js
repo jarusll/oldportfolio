0959b02ce6ed715ffb4e23ceca7415cdcdcc0e66
Suraj yadav <suraj.yadav@recruitcrm.io>
2022-01-16 22:10:41 +0530

    Added url constants

--- constants/urls.js
+++ constants/urls.js
@@ -1,7 +1,9 @@
 const GITHUB_URL = "https://github.com/jarusll";
 const LINKEDIN_URL = "https://www.linkedin.com/in/suraj-yadav-98794b62/";
+const GOOGLE_URL = "mailto:ysuraj877@gmail.com";
 
 export {
     GITHUB_URL,
-    LINKEDIN_URL
+    LINKEDIN_URL,
+    GOOGLE_URL
 };
