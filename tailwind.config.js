72428b950f076bcc2486a823ee43c052b4640269
surajrcrm <suraj.yadav@recruitcrm.io>
2022-01-21 23:09:17 +0530

    disable typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    typography: (theme) => ({}),
+    // typography: (theme) => ({}),
     extend: {},
   },
   plugins: [],
