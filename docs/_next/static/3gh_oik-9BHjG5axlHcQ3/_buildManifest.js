9574ae20b112ad968bcd292e63336c322983c246
surajrcrm <suraj.yadav@recruitcrm.io>
2022-01-21 23:07:13 +0530

    build

--- docs/_next/static/3gh_oik-9BHjG5axlHcQ3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-10b344e839f9071f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/blogs":["static/chunks/pages/blogs-03ee8b33c75b12ad.js"],sortedPages:["/","/_app","/_error","/blogs"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ docs/_next/static/RLRwpAieiq6JYAuZkhNm8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-27b7a84f77b6af74.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/blogs":["static/chunks/pages/blogs-03ee8b33c75b12ad.js"],sortedPages:["/","/_app","/_error","/blogs"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
