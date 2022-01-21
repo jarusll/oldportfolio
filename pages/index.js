85c7a9e79f577acd4a08970b7c1cb1cce382f32a
surajrcrm <suraj.yadav@recruitcrm.io>
2022-01-21 23:05:37 +0530

    fix dark mode hovers

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
     <main className="self-center flex justify-center items-center h-screen w-screen">
       <button className="absolute top-0 right-0 m-1"
         onClick={() => setTheme(theme == "dark" ? "light" : "dark")}>
-        <i class="fas fa-adjust p-2 hover:bg-gray-400 rounded-full transition-colors"></i>
+        <i class="fas fa-adjust p-2 hover:bg-gray-300 dark:hover:bg-gray-800 rounded-full transition-colors"></i>
       </button>
       <article className="grid-cols-1">
         <h1 className="uppercase text-5xl nunito text-center">
@@ -35,8 +35,8 @@ export default function Home() {
               return (
                 <Link href={url} passHref key={url}>
                   <a target="_blank">
-                    <li className="cursor-pointer hover:bg-gray-100">
-                      <i className={`fab fa-3x p-3 rounded ${icon}`}></i>
+                    <li className="cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-900 p-4 rounded-full">
+                      <i className={`fab fa-3x ${icon}`}></i>
                     </li>
                   </a>
                 </Link>
