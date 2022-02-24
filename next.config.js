9d8f39f450cb6dc85069042a7234437e13c34bea
Suraj yadav <suraj.yadav@recruitcrm.io>
2022-02-24 13:42:40 +0530

    ignore fs on client side

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   webpack5: true,
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback.fs = false
+    }
 
     return config;
   },
